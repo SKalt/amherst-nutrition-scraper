421db69598c7b5b9260349d11f9e3cf2a6475cc7
SKalt <skalt16@amherst.edu>
2016-09-07 12:03:56 -0400

    renamed the module, updated setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 
 
 setup(
-    name='acnutrition',
+    name='amherst-nutrition-scraper',
     author='Austin Hartzheim, revised by Steven Kalt',
 
-    version='1.2.1',
-    packages=['wok'],
+    version='1.3.1',
+    packages=['wok-ac'],
     license='GNU GPL v3',
-    description='Download menus from Amherst College\'s NetNutrition website.',
+    description='Download menus and nutrition data from Amherst College\'s NetNutrition website.',
     long_description=open('README.md').read(),
     url='austinhartzheim.me/projects/wok/'
 )
